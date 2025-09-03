c144ce85cb32d1b1d6e7a2599b66a5d21b06e7ea
alexandre.rey <alexandre.rey@ivy.partners>
2025-09-03 20:25:38 +0200

    Add kernel header file and update run script to include kernel execution

--- run.sh
+++ run.sh
@@ -12,4 +12,4 @@ $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
     kernel.c
 
 
-# $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
