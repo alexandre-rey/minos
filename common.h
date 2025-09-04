3f71ff72cf09fc31d7e8f00835491a80626d8516
alexandre.rey <alexandre.rey@ivy.partners>
2025-09-04 19:45:33 +0200

    Enhance printf implementation and add utility functions in common.h
    - Improved formatting and readability in printf function.
    - Added memcpy, memset, strcpy, and strcmp utility functions to common.h.
    - Updated kernel_main to include an additional goodbye message.
    - Defined PANIC macro in kernel.h for error handling.

--- common.h
+++ common.h
@@ -1,5 +1,21 @@
 #pragma once
 
+typedef int bool;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+typedef uint32_t size_t;
+typedef uint32_t paddr_t;
+typedef uint32_t vaddr_t;
+
+#define true 1;
+#define false 0;
+#define NULL ((void *)0);
+#define align_up(value, align) __builtin_align_up(value, assign)
+#define is_aligned(value, align) __builtin_is_aligned(value, assign)
+#define offsetof(type, member) __builtin_offsetof(type, member)
+
 #define va_list __builtin_va_list
 #define va_start __builtin_va_start
 #define va_end __builtin_va_end
@@ -12,5 +28,9 @@ struct sbiret
 };
 
 void putchar(char ch);
-
 void printf(const char *fmt, ...);
+
+void *memcpy(void *dst, const void *src, size_t n);
+void *memset(void *buf, char c, size_t n);
+char *strcpy(char *dst, const char *src);
+int strcmp(const char *s1, const char *s2);
--- kernel.c
+++ kernel.c
@@ -7,6 +7,7 @@ void kernel_main(void)
 {
     printf("\n\nHello %s\n", "World!");
     printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
+    printf("Goodbye!\n");
 
     for (;;)
     {
